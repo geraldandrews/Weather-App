151641e79c0f6955ce3ef11942dc82723447f2f4
Gerald Andrews <gerald2399@comcast.net>
2023-10-15 14:10:20 -0400

    Update get_weather.js

--- serverless/get_weather.js
+++ serverless/get_weather.js
@@ -5,7 +5,7 @@ const { WEATHER_API_KEY } = process.env;
 exports.handler = async (event, context) => {
     const params = JSON.parse(event.body);
     const { lat, lon, units } = params;
-    const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
+    const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={WEATHER_API_KEY}`;
     try {
         const weatherStream = await fetch(url);
         const weatherJson = await weatherStream.json();
